20c75124dd0289f26a1e0cea3f532aeb945c531c
dhruvpattem <dhruv.pattem@gmail.com>
2021-06-22 15:09:51 +0100

    v3

--- build/lib/dxface/entities.py
+++ build/lib/dxface/entities.py
@@ -1,8 +1,8 @@
-from polyline import Polyline
-from lwpolyline import Lwpolyline
-from line import Line
-from circle import Circle
-from arc import Arc
+from .polyline import Polyline
+from .lwpolyline import Lwpolyline
+from .line import Line
+from .circle import Circle
+from .arc import Arc
 
 
 class Entities:
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dxface",
-    version="0.0.1",
+    version="0.0.3",
     author="dhruvnps",
     description="Python interface for AutoCAD DXF",
     long_description=long_description,
